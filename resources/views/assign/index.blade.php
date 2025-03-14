49488df0c25d471fa2da168896a159175b3ac53c
Lemerz <Lemerdngln@gmail.com>
2025-03-14 08:24:53 +0800

    Update index.blade.php

--- resources/views/assign/index.blade.php
+++ resources/views/assign/index.blade.php
@@ -6,11 +6,13 @@
     <title>Assign</title>
 </head>
 <body>
-    <h1>Assigned Items</h1>
-    <a href="{{ route('accountability.create')}}">Add</a>
-    <table border="1">
+<div class = "background"></class> 
+    <h1 class="text-3xl font-bold text-blue-800 mb-6">ASSIGNED ITEMS</h1>
+    <a href="{{ route('accountability.create')}}"class=button>Add</a>
+    <table class="table">
+    
 
-        <thead>
+        <thead class="bg-blue-800 text-white">
             <th>First Name</th>
             <th>Last Name</th>
             <th>Employee Number</th>
@@ -19,7 +21,7 @@
         </thead>
         <tbody>
             @foreach($assigned_items as $item)
-                <tr>
+                <tr class="border-b hover:bg-gray-100">
                     <td>{{ $item['first_name'] }}</td>
                     <td>{{ $item['last_name'] }}</td>
                     <td>{{ $item['employee_number'] }}</td>
@@ -30,6 +32,57 @@
                 </tr>
             @endforeach
         </tbody>
+
     </table>
+
+    <style>
+        .background{
+            margin:0px;
+            padding: 250px;;
+            font-family: sans-serif;
+            background: linear-gradient(#30142b, #2772a1);
+
+        }
+        .table{
+
+            width: 520px;
+            height: 20vh;
+            grid-template-columns: 1fr 1fr;
+            border: 3px solid #00ffff;
+            box-shadow: 0 0 50px 0 #00a6bc;
+        }
+
+        .button{
+        position: relative;
+        display: inline-block;
+        padding: 10px 20px;
+        font-size: 16px;
+        text-decoration: none;
+        text-transform: uppercase;
+        overflow: hidden;
+        margin-top: 5px;
+        letter-spacing: 4px
+        }
+
+        .button{     
+        background: #2772a1;
+        font: 6px;;
+        color: #fff;
+        border-radius: 0px;
+        box-shadow: 0 0 5px #00a6bc,
+                    0 0 10px #00a6bc,
+                    0 0 10px #00a6bc,
+                    0 0 20px #00a6bc;
+        }
+        
+
+
+
+
+        
+        
+
+
+    </style>
 </body>
 </html>
