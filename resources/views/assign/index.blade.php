998d150998117cba4fc9443032c81ace874802b9
ENZ748 <interns@enzconsultancy.com>
2025-03-17 11:25:21 +0800

    index conflict

--- resources/views/assign/index.blade.php
+++ resources/views/assign/index.blade.php
@@ -1,34 +1,36 @@
 @extends('layouts.app') <!-- Extend the master layout -->
 
 @section('content')
-    <div class="container">
-        <h1 class="text-primary">Assigned Items</h1>
-        <a href="{{ route('accountability.create')}}" class="btn btn-primary mb-3">Add Item</a>
-        <div class="table-responsive">
-            <table class="table table-striped">
-                <thead class="bg-primary text-white">
+    <h1>Assigned Items</h1>
+    <a href="{{ route('accountability.create')}}">Add</a>
+    
+    @if($assigned_items->isEmpty())
+        <p>No data found</p> <!-- Display this message when no data is available -->
+    @else
+        <table border="1">
+            <thead>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Employee Number</th>
+                <th>Equipment Name</th>
+                <th>Equipment Details</th>
+                <th>Action</th>
+            </thead>
+            <tbody>
+                @foreach($assigned_items as $item)
                     <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Employee Number</th>
-                        <th>Equipment Name</th>
-                        <th>Action</th>
+                        <td>{{ $item['first_name'] }}</td>
+                        <td>{{ $item['last_name'] }}</td>
+                        <td>{{ $item['employee_number'] }}</td>
+                        <td>{{ $item['equipment_name'] }}</td>
+                        <td>{{ $item['equipment_detail'] }}</td>
+
+                        <td>
+                            <a href="{{route('accountability.edit',$item['id'])}}">Edit</a>
+                        </td>   
                     </tr>
-                </thead>
-                <tbody>
-                    @foreach($assigned_items as $item)
-                        <tr>
-                            <td>{{ $item['first_name'] }}</td>
-                            <td>{{ $item['last_name'] }}</td>
-                            <td>{{ $item['employee_number'] }}</td>
-                            <td>{{ $item['equipment_name'] }}</td>
-                            <td>
-                                <a href="{{route('accountability.edit',$item['id'])}}" class="btn btn-sm btn-primary">Edit</a>
-                            </td>   
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    </div>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
 @endsection
