9cfcbe6b712a316c669fd0cd9c07be3b7bf3f88f
Lemerz <Lemerdngln@gmail.com>
2025-03-18 15:48:41 +0800

    login_navigation

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,7 +41,7 @@
             @endif
 
             <x-primary-button class="ms-3">
-            <div class="button">Sign In</div>
+            <div class="button">login</div>
 
             </x-primary-button>
             </form>
