f86585db00dec749dbfd92fa944db6270f490dd2
Lemerz <Lemerdngln@gmail.com>
2025-03-28 15:24:37 +0800

    navi_welcome new

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,46 +11,20 @@
                 </div>
             </div>
 <body>           
-        <!-- Flag Selector -->
-        <div class="flag-selector">
-            <div class="flag-container">
-                <div class="flag-button flag-australia" data-flag="australia" data-country="Australia" onclick="changeBackground('australia')"></div>
-                <span class="flag-name">Australia</span>
-            </div>
-            <div class="flag-container">
-                <div class="flag-button flag-canada" data-flag="canada" data-country="Canada" onclick="changeBackground('canada')"></div>
-                <span class="flag-name">Canada</span>
-            </div>
-            <div class="flag-container">
-                <div class="flag-button flag-newzealand" data-flag="newzealand" data-country="New Zealand" onclick="changeBackground('newzealand')"></div>
-                <span class="flag-name">New Zealand</span>
-            </div>
-            <div class="flag-container">
-                <div class="flag-button flag-germany" data-flag="germany" data-country="Germany" onclick="changeBackground('germany')"></div>
-                <span class="flag-name">Germany</span>
-            </div>
-            <div class="flag-container">
-                <div class="flag-button flag-ireland" data-flag="ireland" data-country="Ireland" onclick="changeBackground('ireland')"></div>
-                <span class="flag-name">Ireland</span>
-            </div>
-            <div class="flag-container">
-                <div class="flag-button flag-unitedkingdom" data-flag="unitedkingdom" data-country="United Kingdom" onclick="changeBackground('unitedkingdom')"></div>
-                <span class="flag-name">UK</span>
-            </div>
-        </div>
 
             <!-- Social Media Icons Container -->
             <div class="flex items-center space-x-1"> <!-- More compact spacing -->
-                <!-- TikTok -->
-                <div class="social-icon">
-                    <a href="https://www.tiktok.com/@enz.consultancy" target="_blank">
-                        <button class="button btn-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 48 48" fill="#1e90ff"> 
-                                <path d="M38.0766847,15.8542954 C36.0693906,15.7935177 34.2504839,14.8341149 32.8791434,13.5466056 C32.1316475,12.8317108 31.540171,11.9694126 31.1415066,11.0151329 C30.7426093,10.0603874 30.5453728,9.03391952 30.5619062,8 L24.9731521,8 L24.9731521,28.8295196 C24.9731521,32.3434487 22.8773693,34.4182737 20.2765028,34.4182737 C19.6505623,34.4320127 19.0283477,34.3209362 18.4461858,34.0908659 C17.8640239,33.8612612 17.3337909,33.5175528 16.8862248,33.0797671 C16.4386588,32.6422142 16.0833071,32.1196657 15.8404292,31.5426268 C15.5977841,30.9658208 15.4727358,30.3459348 15.4727358,29.7202272 C15.4727358,29.0940539 15.5977841,28.4746337 15.8404292,27.8978277 C16.0833071,27.3207888 16.4386588,26.7980074 16.8862248,26.3604545 C17.3337909,25.9229017 17.8640239,25.5791933 18.4461858,25.3491229 C19.0283477,25.1192854 19.6505623,25.0084418 20.2765028,25.0219479 C20.7939283,25.0263724 21.3069293,25.1167239 21.794781,25.2902081 L21.794781,19.5985278 C21.2957518,19.4900128 20.7869423,19.436221 20.2765028,19.4380839 C18.2431278,19.4392483 16.2560928,20.0426009 14.5659604,21.1729264 C12.875828,22.303019 11.5587449,23.9090873 10.7814424,25.7878401 C10.003907,27.666593 9.80084889,29.7339663 10.1981162,31.7275214 C10.5953834,33.7217752 11.5748126,35.5530237 13.0129853,36.9904978 C14.4509252,38.4277391 16.2828722,39.4064696 18.277126,39.8028054 C20.2711469,40.1991413 22.3382874,39.9951517 24.2163416,39.2169177 C26.0948616,38.4384508 27.7002312,37.1209021 28.8296253,35.4300711 C29.9592522,33.7397058 30.5619062,31.7522051 30.5619062,29.7188301 L30.5619062,18.8324027 C32.7275484,20.3418321 35.3149087,21.0404263 38.0766847,21.0867664 L38.0766847,15.8542954 Z"></path>
+                   <!-- Facebook -->
+                   <div class="social-icon">
+                    <a href="https://www.facebook.com/enzecs" target="_blank">
+                        <button class="button btn-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512" fill="#1e90ff">
+                                <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path>
                             </svg>
                         </button>
                     </a>
                 </div>
+                
 
                 <!-- Instagram -->
                 <div class="social-icon">
@@ -73,17 +47,18 @@
                         </button>
                     </a>
                 </div>
-
-                <!-- Facebook -->
+                
+                <!-- TikTok -->
                 <div class="social-icon">
-                    <a href="https://www.facebook.com/enzecs" target="_blank">
-                        <button class="button btn-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512" fill="#1e90ff">
-                                <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path>
+                    <a href="https://www.tiktok.com/@enz.consultancy" target="_blank">
+                        <button class="button btn-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 48 48" fill="#1e90ff"> 
+                                <path d="M38.0766847,15.8542954 C36.0693906,15.7935177 34.2504839,14.8341149 32.8791434,13.5466056 C32.1316475,12.8317108 31.540171,11.9694126 31.1415066,11.0151329 C30.7426093,10.0603874 30.5453728,9.03391952 30.5619062,8 L24.9731521,8 L24.9731521,28.8295196 C24.9731521,32.3434487 22.8773693,34.4182737 20.2765028,34.4182737 C19.6505623,34.4320127 19.0283477,34.3209362 18.4461858,34.0908659 C17.8640239,33.8612612 17.3337909,33.5175528 16.8862248,33.0797671 C16.4386588,32.6422142 16.0833071,32.1196657 15.8404292,31.5426268 C15.5977841,30.9658208 15.4727358,30.3459348 15.4727358,29.7202272 C15.4727358,29.0940539 15.5977841,28.4746337 15.8404292,27.8978277 C16.0833071,27.3207888 16.4386588,26.7980074 16.8862248,26.3604545 C17.3337909,25.9229017 17.8640239,25.5791933 18.4461858,25.3491229 C19.0283477,25.1192854 19.6505623,25.0084418 20.2765028,25.0219479 C20.7939283,25.0263724 21.3069293,25.1167239 21.794781,25.2902081 L21.794781,19.5985278 C21.2957518,19.4900128 20.7869423,19.436221 20.2765028,19.4380839 C18.2431278,19.4392483 16.2560928,20.0426009 14.5659604,21.1729264 C12.875828,22.303019 11.5587449,23.9090873 10.7814424,25.7878401 C10.003907,27.666593 9.80084889,29.7339663 10.1981162,31.7275214 C10.5953834,33.7217752 11.5748126,35.5530237 13.0129853,36.9904978 C14.4509252,38.4277391 16.2828722,39.4064696 18.277126,39.8028054 C20.2711469,40.1991413 22.3382874,39.9951517 24.2163416,39.2169177 C26.0948616,38.4384508 27.7002312,37.1209021 28.8296253,35.4300711 C29.9592522,33.7397058 30.5619062,31.7522051 30.5619062,29.7188301 L30.5619062,18.8324027 C32.7275484,20.3418321 35.3149087,21.0404263 38.0766847,21.0867664 L38.0766847,15.8542954 Z"></path>
                             </svg>
                         </button>
                     </a>
                 </div>
+             
 
                 <!-- LinkedIn -->
                 <div class="social-icon">
@@ -95,6 +70,56 @@
                         </button>
                     </a>
                 </div>
+
+
+                <nav x-data="{ 
+    open: false, 
+    darkMode: localStorage.getItem('darkMode') === 'true',
+    toggleDarkMode() {
+        this.darkMode = !this.darkMode;
+        localStorage.setItem('darkMode', this.darkMode);
+        document.documentElement.classList.toggle('dark', this.darkMode);
+    }
+}" 
+class="bg-blue" 
+:class="{ 'dark': darkMode }">
+    <!-- Previous code remains unchanged -->
+
+    <!-- Social Media Icons Container -->
+    <div class="flex items-center space-x-1">
+        <!-- Previous social media icons -->
+        
+        <!-- LinkedIn Icon removed -->
+
+        <!-- Dark/Light Mode Toggle -->
+        <div class="social-icon">
+            <button 
+                @click="toggleDarkMode()" 
+                class="button btn-dark-mode"
+                :aria-label="darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'"
+            >
+                <svg 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    height="1em" 
+                    viewBox="0 0 24 24" 
+                    :fill="darkMode ? '#FFD700' : '#333'"
+                >
+                    <path 
+                        x-show="!darkMode" 
+                        d="M12 3c-4.97 0-9 4.03-9 9s4.03 9 9 9 9-4.03 9-9c0-.46-.04-.92-.1-1.36-.98 1.37-2.58 2.26-4.4 2.26-3.08 0-5.57-2.5-5.57-5.57 0-1.82.89-3.42 2.26-4.4C12.92 3.04 12.46 3 12 3z"
+                    />
+                    <path 
+                        x-show="darkMode" 
+                        d="M12 7c-2.76 0-5 2.24-5 5s2.24 5 5 5 5-2.24 5-5-2.24-5-5-5zM2 12.5c-.28 0-.5.22-.5.5s.22.5.5.5h2c.28 0 .5-.22.5-.5s-.22-.5-.5-.5H2zm18 0c-.28 0-.5.22-.5.5s.22.5.5.5h2c.28 0 .5-.22.5-.5s-.22-.5-.5-.5h-2zM12 2c-.28 0-.5.22-.5.5v2c0 .28.22.5.5.5s.5-.22.5-.5v-2c0-.28-.22-.5-.5-.5zm0 18c-.28 0-.5.22-.5.5v2c0 .28.22.5.5.5s.5-.22.5-.5v-2c0-.28-.22-.5-.5-.5zM5.15 4.15c-.2-.2-.51-.2-.71 0l-1.5 1.5c-.2.2-.2.51 0 .71.2.2.51.2.71 0l1.5-1.5c.2-.2.2-.51 0-.71zm12 12c-.2-.2-.51-.2-.71 0l-1.5 1.5c-.2.2-.2.51 0 .71.2.2.51.2.71 0l1.5-1.5c.2-.2.2-.51 0-.71zm-12 0c.2-.2.51-.2.71 0l1.5 1.5c.2.2.2.51 0 .71-.2.2-.51.2-.71 0l-1.5-1.5c-.2-.2-.2-.51 0-.71zm12-12c.2-.2.51-.2.71 0l1.5 1.5c.2.2.2.51 0 .71-.2.2-.51.2-.71 0l-1.5-1.5c-.2-.2-.2-.51 0-.71z"
+                    />
+                </svg>
+            </button>
+        </div>
+
+        <!-- Rest of the existing code remains unchanged -->
+    </div>
+</nav>
+
 
                 <!-- Admin Profile Dropdown -->
                 <div class="ml-2 hidden sm:flex sm:items-center">
@@ -219,58 +244,7 @@
             width: 24px;
             height: 24px;
         }
-        /* Flag Selector */
-        .flag-selector {
-            display: flex;
-            gap: 50px;
-            align-items: center;
-        }
-
-        .flag-container {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            gap: 5px;
-        }
-
-        .flag-button {
-            width: 40px;
-            height: 40px;
-            border-radius: 50%;
-            background-size: cover;
-            cursor: pointer;
-            transition: all 0.3s ease;
-            border: 2px solid transparent;
-        }
-
-        .flag-button:hover {
-            transform: scale(1.1);
-            border-color: #007bff;
-        }
-
-        .flag-button.active {
-            border-color: #007bff;
-            box-shadow: 0 0 0 3px rgba(0,123,255,0.3);
-        }
-
-        .flag-name {
-            font-size: 13px;
-            color: #333;
-            text-align: center;
-        }
-
-        /* Flag SVGs (kept from previous implementation) */
-        .flag-australia { background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 60 60'%3E%3Ccircle cx='30' cy='30' r='30' fill='%230052B4'/%3E%3Cpath fill='%23FFF' d='M30 0l15 25.98-15 25.98L15 25.98z'/%3E%3Cpath fill='%23C8102E' d='M0 30l30-15 30 15-30 15z'/%3E%3C/svg%3E"); }
-        .flag-canada { background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 60 60'%3E%3Ccircle cx='30' cy='30' r='30' fill='%23FFF'/%3E%3Cpath fill='%23FF0000' d='M0 0h25v60H0zM35 0h25v60H35z'/%3E%3Cpath fill='%23FF0000' d='M30 15l-6 9h12z M30 45l-6-9h12z'/%3E%3C/svg%3E"); }
-        .flag-newzealand { background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 60 60'%3E%3Ccircle cx='30' cy='30' r='30' fill='%23012169'/%3E%3Cpath fill='%23FFF' d='M0 0l30 15L0 30z'/%3E%3Cpath fill='%23C8102E' d='M0 0h15l45 60h-15zM60 0h-15L0 60h15z'/%3E%3Cpath fill='%23FFF' d='M24.75 21l-2.25-6.75-2.25 6.75h-7.5l6 4.5-2.25 6.75 6-4.5 6 4.5-2.25-6.75 6-4.5z'/%3E%3C/svg%3E"); }
-        .flag-germany { background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 60 60'%3E%3Ccircle cx='30' cy='30' r='30' fill='%23000000'/%3E%3Crect width='60' height='20' y='0' fill='%23DD0000'/%3E%3Crect width='60' height='20' y='20' fill='%23FFCC00'/%3E%3Crect width='60' height='20' y='40' fill='%23FF0000'/%3E%3C/svg%3E"); }
-        .flag-ireland { background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 60 60'%3E%3Ccircle cx='30' cy='30' r='30' fill='%23FFF'/%3E%3Crect width='20' height='60' fill='%23169B62'/%3E%3Crect width='20' height='60' x='40' fill='%23FF883E'/%3E%3C/svg%3E"); }
-        .flag-unitedkingdom { background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 60 60'%3E%3Crect width='60' height='60' fill='%23012169'/%3E%3Cpath fill='%23FFF' d='M30 0v30h30v-30z'/%3E%3Cpath fill='%23FFF' d='M0 0v30h30v-30z'/%3E%3Cpath fill='%23C8102E' d='M0 0l30 30h30l-30-30z'/%3E%3Cpath fill='%23C8102E' d='M0 30l30-30h30l-30 30z'/%3E%3Cpath fill='%23C8102E' d='M30 0l-15 15h10v30h10v-30h10z'/%3E%3Cpath fill='%23C8102E' d='M30 30h15v10l-15-10z'/%3E%3C/svg%3E"); }
-    /* General navigation styling */
-    nav {
-        min-height: 56px; /* Fixed minimum height */
-        max-height: 56px; /* Fixed maximum height */
-    }
+    
     
     /* Social media icons styling */
     .social-icon {
@@ -386,6 +360,53 @@
     .Btn:active {
         transform: translate(1px, 1px); /* Smaller movement */
     }
+  /* Base styles */
+  body {
+        background: linear-gradient(to right, #449DD1, rgb(243, 249, 252));
+        color: #333;
+        transition: background 0.3s ease, color 0.3s ease;
+    }
+
+    /* Dark mode styles */
+    body.dark {
+        background: linear-gradient(to right, #1a2634, #0f2027);
+        color: #f4f4f4;
+    }
+
+    /* Transition for all elements */
+    body,
+    body * {
+        transition: background 0.3s ease, 
+                   color 0.3s ease, 
+                   border-color 0.3s ease;
+    }
+
+    /* Dark mode specific styles */
+    body.dark nav {
+        background-color: #1a2634;
+    }
+
+    body.dark .social-icon:hover {
+        background-color: rgba(255, 255, 255, 0.1);
+    }
+
+    body.dark .profile-btn {
+        color: #f4f4f4;
+        border-left-color: #444;
+    }
+
+    body.dark .Btn {
+        background-color: #2c3e50;
+        color: #ecf0f1;
+    }
+
+    body.dark .Btn .sign svg path {
+        fill: #ecf0f1;
+    }
+
+    body.dark .Btn:hover {
+        background-color: #34495e;
+    }
 </style>
 
 <script>
@@ -407,4 +428,16 @@
 
         // Set initial background to first flag
         changeBackground('australia');
+
+        // Dark mode persistence using localStorage
+    document.addEventListener('alpine:init', () => {
+        Alpine.store('darkMode', {
+            on: localStorage.getItem('darkMode') === 'true',
+            toggle() {
+                this.on = !this.on;
+                localStorage.setItem('darkMode', this.on);
+                document.documentElement.classList.toggle('dark', this.on);
+            }
+        });
+    });
     </script>
